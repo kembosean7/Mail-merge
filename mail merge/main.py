48bc06c00506dd449208f0173a8b9fa2ab1bd33a
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2024-11-07 18:00:54 +0200

    generate personalised email for names in txt file

--- mail merge/main.py
+++ mail merge/main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 #TODO: Create a letter using starting_letter.docx 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
@@ -6,3 +9,52 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+def get_files(filename):
+
+    try :
+        with open(filename, 'r', errors= 'ignore') as f :
+            contents = f.read()
+            return contents
+    except FileNotFoundError:
+        print("File not found.")
+
+    
+def get_names(filename):
+
+    try:
+        with open(filename, 'r', errors= 'ignore') as f :
+            data = f.read().splitlines()
+            return data
+    except FileNotFoundError:
+        print("File not found.")
+    
+
+
+def save_letters(letter,names):
+
+    os.makedirs("ReadyToSend", exist_ok=True )
+
+    for name in names :
+        personal_letter = letter.replace("[name]", name)
+        filename = f"{name}.txt"
+        file_path =  os.path.join(r'C:\Users\LENOVO\Mail-merge\mail merge\ReadyToSend', filename)
+        with open(file_path, 'w') as f:
+        # with open((os.path.join('/path/to/Documents',completeName), "w"))
+            f.write(personal_letter)
+
+
+        
+
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Invalid usage, too little arguments.")
+        sys.exit()
+
+    letter = get_files(sys.argv[1])
+    names = get_names(sys.argv[2])
+    save_letters(letter,names)
+
+main()
